1c51636b5a98ca1b1ab4cbbea830313903526c87
Saeid Taheri Fard <saeidtf@gmail.com>
2022-12-27 18:45:23 +0330

    change checkToken security

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,30 @@
 const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+const { errorRequest } = require("../utilities/util");
 
 const checkToken = (req, res, next) => {
   const token = req.headers["authorization"];
   if (!token) {
-    return res.status(403).send({
-      success: false,
-      message: "No token provided.",
-    });
+    return errorRequest(res, "No token provided", 403);
   }
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(500).send({
-        success: false,
-        message: "Failed to authenticate token.",
-      });
+      return errorRequest(res, "Failed to authenticate token", 500);
     }
-    req.userId = decoded.id;
+    const user = User.findOne({
+      where: {
+        id: decoded.id,
+        email: decoded.email,
+      },
+    });
+    if (!user) {
+      return errorRequest(res, "No token provided", 403);
+    }
+    req.userId = decoded.id;    
     next();
   });
 };
 
-
 module.exports = {
-    checkToken,
+  checkToken,
 };
