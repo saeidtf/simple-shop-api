9ab2001e26b73d838b940c86066b8f5a75de0538
Saeid Taheri Fard <saeidtf@gmail.com>
2022-12-27 18:40:20 +0330

    env port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const helmet = require('helmet')
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT ||  3000;
 
 app.disable("x-powered-by");
 app.use(express.json());
