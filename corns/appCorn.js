ec20919d2925c35f2acd6751d5df581930e8af7a
Saeid Taheri Fard <saeidtf@gmail.com>
2023-01-26 11:04:50 +0330

    change corn time

--- corns/appCorn.js
+++ corns/appCorn.js
@@ -1,7 +1,7 @@
 const cornJob = require("node-cron");
 const { User, Order, OrderItem } = require("../models");
 exports.initScheduledJobs = () => {
-  const scheduledJobFunction = cornJob.schedule("*/5 * * * *", () => {
+  const scheduledJobFunction = cornJob.schedule("*/25 * * * *", () => {
     console.log("I'm executed on a schedule!");
     // delete all order items
     OrderItem.destroy({ truncate: true })
